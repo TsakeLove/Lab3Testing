a4687ba94c5042b267ab2164187cccf47cad57d4
Ihor <tsake.love@gmail.com>
2021-04-01 05:20:36 +0300

    asd

--- src/test/java/TestWithSeleniumPages.java
+++ src/test/java/TestWithSeleniumPages.java
@@ -24,7 +24,7 @@ public class TestWithSeleniumPages {
     private static final String trueLink = "hromadske.ua";
     private static final String err = "Мы заметили несколько необычных попыток входа в вашу учетную запись. С целью обеспечения ее безопасности просим вас ввести свой номер телефона или имя пользователя, чтобы подтвердить, что это действительно вы.";
     private static final String wrongData = "Введенные адрес электронной почты и пароль не совпадают с сохраненными в нашей базе данных. Проверьте правильность введенных данных и повторите попытку.";
-    private static final long timeOutInSeconds = 20;
+    private static final long timeOutInSeconds = 10;
     private WebDriverWait waiter;
     private WebDriver driver;
     @Before
@@ -37,25 +37,26 @@ public class TestWithSeleniumPages {
 
     @Test
     public void testAuthName() {
-//
-//        WebDriver driver =  new ChromeDriver();
-//        //driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-//        WebDriverWait waiter = createWebDriverWait(driver);
 
-        LoginPage loginPage = new LoginPage(driver, waiter);
-        loginPage.loginValidUser(email, password, email2);
+        driver.get("https://allo.ua");
+        waiter.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"search-form__input\"]")));
+        driver.findElement(By.xpath("//*[@id=\"search-form__input\"]")).click();
+        driver.findElement(By.xpath("//*[@id=\"search-form__input\"]")).sendKeys("Xiaomi Mi 11 8/256GB Horizon Blue");
+        driver.findElement(By.xpath("//*[@id=\"__layout\"]/div/div[2]/div[1]/div[3]/div/ul/li[4]/div/div/form/button[3]")).click();
+        waiter.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"__layout\"]/div/div[2]/h1")));
 
+        String a = driver.findElement(By.xpath("//*[@id=\"__layout\"]/div/div[2]/h1")).getText();
 
-        //loginPage.loginCheck();
-
-     //   String name = driver.findElement(By.cssSelector("#react-root > div > div > div.css-1dbjc4n.r-18u37iz.r-13qz1uu.r-417010 > main > div > div > div > div > div > div.css-1dbjc4n.r-aqfbo4.r-14lw9ot.r-j7yic.r-rull8r.r-qklmqi.r-gtdqiz.r-1gn8etr.r-1g40b8q > div.css-1dbjc4n.r-1loqt21.r-136ojw6 > div > div > div > div > div.css-1dbjc4n.r-16y2uox.r-1wbh5a2.r-1pi2tsx.r-1777fci > div > h2 > div > div > div > span.css-901oao.css-16my406.css-bfa6kz.r-18jsvk2.r-poiln3.r-1vr29t4.r-bcqeeo.r-3s2u2q.r-qvutc0 > span > span")).getText();
-        Assert.assertEquals("Data Test", "Data Test");
+        Assert.assertEquals(a, "Результати пошуку для 'Xiaomi Mi 11 8/256GB Horizon Blue'");
         driver.quit();
     }
 
-//    @Test
-//    public void testLastTwit() {
-//        WebDriver driver = createWebDriver();
+    @Test
+    public void testLastTwit() {
+        driver.get("https://allo.ua/ua/products/mobile/xiaomi-mi-11-8-256gb-horizon-blue.html");
+        waiter.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#__layout > div > div.page__content.page__content--inverse > div.product-view > div.p-view__header.product-view__header > h1")));
+
+        //        WebDriver driver = createWebDriver();
 //        WebDriverWait waiter = createWebDriverWait(driver);
 //
 //        LoginPage loginPage = new LoginPage(driver, waiter);
@@ -71,55 +72,52 @@ public class TestWithSeleniumPages {
 //        driver.findElement(By.xpath("//*[@id=\"react-root\"]/div/div/div[2]/main/div/div/div/div/div/div[2]/div/div[2]/div[1]/div/div/div/div[2]/div[4]/div/div/div[2]/div[3]")).click();
 //        waiter.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"react-root\"]/div/div/div[2]/main/div/div/div/div/div/div[4]/div/div/section/div/div/div[1]/div/div/article/div/div/div/div[2]/div[2]/div[2]/div[1]/div/span")));
 //        String lastTwit = driver.findElement(By.xpath("//*[@id=\"react-root\"]/div/div/div[2]/main/div/div/div/div/div/div[4]/div/div/section/div/div/div[1]/div/div/article/div/div/div/div[2]/div[2]/div[2]/div[1]/div/span")).getText();
+
+        Assert.assertNotEquals("Hello", "!Hello");
+
+        driver.quit();
+    }
 //
-//        Assert.assertNotEquals("Hello , " + hello, lastTwit);
+    @Test
+    public void testLinkText() {
+        driver.get("https://allo.ua/ua/products/mobile/xiaomi-mi-11-8-256gb-horizon-blue.html");
+        waiter.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"product-buy-button\"]")));
+        driver.findElement(By.xpath("//*[@id=\"product-buy-button\"]")).click();
+        waiter.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("/html/body/div[4]/div/div/div[3]/div/div[1]/div/div/div[1]/div/ul/li/div/div[1]/div[2]/div[2]/div[2]/div")));
+        String a = driver.findElement(By.xpath(" /html/body/div[4]/div/div/div[3]/div/div[1]/div/div/div[1]/div/ul/li/div/div[1]/div[2]/div[1]/a/p/span")).getText();
+
+        Assert.assertEquals(a, "Xiaomi Mi 11 8/256GB Horizon Blue");
+        driver.quit();
+    }
 //
-//        driver.quit();
-//    }
-//
-//    @Test
-//    public void testLinkText() {
-//        WebDriver driver = createWebDriver();
-//        WebDriverWait waiter = createWebDriverWait(driver);
-//
-//        driver.get("https://twitter.com/HromadskeUA");
-//
-//        waiter.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#react-root > div > div > div.css-1dbjc4n.r-18u37iz.r-13qz1uu.r-417010 > main > div > div > div > div > div > div:nth-child(2) > div > div > div:nth-child(1) > div.css-1dbjc4n.r-1ifxtd0.r-ymttw5.r-ttdzmv > div.css-1dbjc4n.r-6gpygo.r-14gqq1x > div > div > div.css-1dbjc4n.r-1awozwy.r-18u37iz.r-dnmrzs > div > span:nth-child(1) > span")));
-//        String errorLink = driver.findElement(By.linkText("hromadske.ua/donate")).getText();
-//        Assert.assertEquals(trueLink, errorLink);
-//        driver.quit();
-//    }
-//
-//    @Test
-//    public void testErrorText() {
-//        WebDriver driver = createWebDriver();
-//        WebDriverWait waiter = createWebDriverWait(driver);
-//
-//        LoginPage loginPage = new LoginPage(driver, waiter);
-//        loginPage.loginForCheck(email, password);
-//
-//        waiter.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#react-root > div > div > div.css-1dbjc4n.r-13qz1uu.r-417010 > main > div > div > div:nth-child(1) > div > span")));
-//
-//        String willSee = driver.findElement(By.cssSelector("#react-root > div > div > div.css-1dbjc4n.r-13qz1uu.r-417010 > main > div > div > div:nth-child(1) > div > span")).getText();
-//        Assert.assertEquals(willSee, err);
-//
-//
-//        driver.quit();
-//    }
-//
-//    @Test
-//    public void testSignUpWithWrongEmail() {
-//        WebDriver driver = createWebDriver();
-//        WebDriverWait waiter = createWebDriverWait(driver);
-//
-//        LoginPage loginPage = new LoginPage(driver, waiter);
-//        loginPage.loginForCheck(wrongEmail, password);
-//        waiter.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#react-root > div > div > div.css-1dbjc4n.r-13qz1uu.r-417010 > main > div > div > div:nth-child(3) > div > div > a:nth-child(1)")));
-//
-//        String trueData = driver.findElement(By.cssSelector("#react-root > div > div > div.css-1dbjc4n.r-13qz1uu.r-417010 > main > div > div > div:nth-child(1) > div > div > div > div > div > div")).getText();
-//        Assert.assertEquals(trueData, wrongData);
-//        driver.quit();
-//    }
+    @Test
+    public void testErrorText() {
+        driver.get("https://allo.ua/ua/products/mobile/xiaomi-mi-11-8-256gb-horizon-blue.html");
+        waiter.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"product-buy-button\"]")));
+        driver.findElement(By.xpath("//*[@id=\"__layout\"]/div/div[2]/div[1]/div[1]/div/div[2]/div/a/span")).click();
+        waiter.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"p-comments\"]/div/div[1]/div[2]/div[1]/div[1]")));
+        String a = driver.findElement(By.xpath("//*[@id=\"p-comments\"]/div/div[1]/div[2]/div[1]/div[1]")).getText();
+
+        Assert.assertEquals(a, "4.4");
+
+        driver.quit();
+    }
+
+    @Test
+    public void testSignUpWithWrongEmail() {
+
+        driver.get("https://allo.ua");
+        waiter.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"search-form__input\"]")));
+        driver.findElement(By.xpath("//*[@id=\"search-form__input\"]")).click();
+        driver.findElement(By.xpath("//*[@id=\"search-form__input\"]")).sendKeys("Xiaomi Mi 11 8/256GB Horizon Blue");
+        driver.findElement(By.xpath("//*[@id=\"__layout\"]/div/div[2]/div[1]/div[3]/div/ul/li[4]/div/div/form/button[3]")).click();
+        waiter.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"__layout\"]/div/div[2]/h1")));
+        String a = driver.findElement(By.xpath("//*[@id=\"filter_id-1822\"]/p/span")).getText();
+
+
+        Assert.assertEquals(a, "(1)");
+        driver.quit();
+    }
 
 
     @After
